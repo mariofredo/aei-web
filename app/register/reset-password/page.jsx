ef0cb88d1546d5365cd22ba0e7c525b6151fcdb1
Dev-FE-Superfk <dev-fe@superfk.co>
2024-12-10 20:02:31 +0700

    renama path

--- app/register/reset-password/page.jsx
+++ app/register/reset-password/page.jsx
@@ -1,7 +1,7 @@
 'use client';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { useEffect, useState, useRef, Suspense } from 'react';
-import '../../styles/completeProfile.scss';
+import '../../../styles/completeProfile.scss';
 
 function ResetPassword() {
     const searchParams = useSearchParams();
